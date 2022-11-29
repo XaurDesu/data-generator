1536c79d690335196f33470a666daaf519961037
Jaime Andres Torres Bermejo <andrestbermejoj@gmail.com>
2022-11-29 15:39:07 -0500

    add script

---
+++ src/script.py
@@ -0,0 +1,28 @@
+from faker import Faker
+import random
+
+NUM_ENTRIES = 1000001
+
+fake = Faker()
+
+def generate_compra():
+    ret = []
+
+    states = ['CREADA','PAGADA','CANCELADA']
+    for i in range(NUM_ENTRIES):
+        ret.append("INSERT INTO COMPRA (ID,VALOR_COMPRA_TOTAL,URL_FACTURA_ELECTRONICA,ESTADO_COMPRA,COMPRADOR,FECHA_COMPRA("
+        +str(i)+','
+        +str(random.randint(1000,3500000))+','
+        +fake.domain_name()+','
+        +random.choice(states)+','
+        +fake.date())
+    return ret
+
+def main():
+    print("Processing your mock data...")    
+    compras = generate_compra()
+    print(compras)
+    print("Done! Enjoy.")
+
+if __name__ == "__main__":
+    main()
